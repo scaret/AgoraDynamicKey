2b589590eb56b8e048b06d2293917e17f5448164
Yao Ximing <cmd100000@gmail.com>
2018-03-23 17:05:46 +0800

    delete test code

--- java/sample/io/agora/media/sample/BuilderTokenSample.java
+++ java/sample/io/agora/media/sample/BuilderTokenSample.java
@@ -3,8 +3,6 @@ package io.agora.media.sample;
 import io.agora.media.AccessToken;
 import io.agora.media.SimpleTokenBuilder;
 
-import java.util.TreeMap;
-
 public class BuilderTokenSample {
     static String appId = "970CA35de60c44645bbae8a215061b33";
     static String appCertificate = "5CFd2fd1755d40ecb72977518be15d3b";
@@ -19,8 +17,6 @@ public class BuilderTokenSample {
         token.setPrivilege(AccessToken.Privileges.kPublishAudioStream, expireTimestamp);
         token.setPrivilege(AccessToken.Privileges.kPublishVideoStream, expireTimestamp);
         token.setPrivilege(AccessToken.Privileges.kPublishDataStream, expireTimestamp);
-        TreeMap<Short, Integer> test = token.mTokenCreator.message.messages;
-        System.out.println("");
 
         String result = token.buildToken();
         System.out.println(result);
